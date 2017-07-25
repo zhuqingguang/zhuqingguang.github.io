5272b49330fe2e9b599685d0733806b8d972c095
zhuqingguang <vac872089248@gmail.com>
2017-07-25 13:07:39 +0800

    更改代码高亮

--- tensent.js
+++
@@ -1,9 +0,0 @@
-var _mtac = {};
-(function() {
-    var mta = document.createElement("script");
-    mta.src = "https://pingjs.qq.com/h5/stats.js?v2.0.2";
-    mta.setAttribute("name", "MTAH5");
-    mta.setAttribute("sid", "500461079");
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(mta, s);
-})();
